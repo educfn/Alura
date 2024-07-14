163c6352bece432cb0078e3331c4a8ddc4f63c0c
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-07-14 08:51:58 -0300

    Seção 4 - Desenvolvendo a classe banda

---
+++ C#AplicandoOrientacaoObjetos/ScreenSound/Banda.cs
@@ -0,0 +1,22 @@
+namespace CsharpAplicandoOrientacaoObjetos.ScreenSound
+{
+    class Banda
+    {
+        private List<Album> albums = new List<Album>();
+        public string Nome { get; set; }
+
+        public void AdicionarAlbum(Album album)
+        {
+            albums.Add(album);
+        }
+
+        public void ExibirDiscografia()
+        {
+            Console.WriteLine($"Discografia da banda {Nome}");
+            foreach (var album in albums)
+            {
+                Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");
+            }
+        }
+    }
+}
